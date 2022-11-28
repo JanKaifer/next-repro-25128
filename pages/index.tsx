6b9dd756ced2acea73df9d3a03b3d1bc1cdb1d44
Jan Kaifer <jan@kaifer.cz>
2022-11-28 19:20:28 +0100

    added repro

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,19 @@
-/** Add your relevant code here for the issue to reproduce */
+import Image from "next/image";
+
 export default function Home() {
-  return null
+  return <div>
+    <Image
+      src="https://apiscouternase.cdn.triggerfish.cloud/uploads/2021/03/Scouter_bygger_bro_Foto_Jenny_Modess_1800x1200.jpg"
+      width="1800"
+      height="1200"
+      alt="img"
+    />
+    <Image
+      src="https://apiscouternase.cdn.triggerfish.cloud/uploads/2021/03/Scouter_bygger_bro_Foto_Jenny_Modess_1800x1200.jpg"
+      width="1800"
+      height="1200"
+      sizes="100vw"
+      alt="img"
+    />
+  </div>
 }
